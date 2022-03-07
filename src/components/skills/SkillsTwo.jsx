e672c7c4e3a88d42df7cc0e792aead1e20501ee0
jdijaili <jennifer.dijailirx@gmail.com>
2022-03-06 21:56:25 -0800

    updated skills

--- src/components/skills/SkillsTwo.jsx
+++ src/components/skills/SkillsTwo.jsx
@@ -13,13 +13,12 @@ const SkillsTwo = () => {
                 data-aos-duration="1200"
               >
                 <div className="shane_tm_title">
-                  <h3>I have high skills in developing and programming</h3>
+                  <span>Skills</span>
+                  <h3>Tech Stack</h3>
                 </div>
                 <div className="text">
                   <p>
-                    I am working on a professional, visually sophisticated and
-                    technologically proficient, responsive and multi-functional
-                    personal portfolio template Shane.
+                    I'm currently developing full stack apps with the following technologies:
                   </p>
                 </div>
               </div>
@@ -33,49 +32,49 @@ const SkillsTwo = () => {
                 <div className="tokyo_progress">
                   <div className="progress_inner">
                     <span>
-                      <span className="label">Web Development</span>
-                      <span className="number">95%</span>
+                      <span className="label">Languages</span>
                     </span>
                     <div className="background">
                       <div className="bar">
-                        <div
-                          className="bar_in"
-                          style={{ width: 95 + "%" }}
-                        ></div>
                       </div>
                     </div>
+                    <p style={{ marginTop: "5px" }} >Javascript | Python | HTML | CSS</p>
                   </div>
                   {/* End .progress_inner */}
 
                   <div className="progress_inner">
                     <span>
-                      <span className="label">Brand Identity</span>
-                      <span className="number">80%</span>
+                      <span className="label">ORM/RBMS</span>
                     </span>
                     <div className="background">
                       <div className="bar">
-                        <div
-                          className="bar_in"
-                          style={{ width: 80 + "%" }}
-                        ></div>
                       </div>
                     </div>
+                    <p style={{ marginTop: "5px" }} >Sequelize | SQLAlchemy | PostgreSQL</p>
                   </div>
                   {/* End .progress_inner */}
 
                   <div className="progress_inner">
                     <span>
-                      <span className="label">Logo Design</span>
-                      <span className="number">90%</span>
+                      <span className="label">Frameworks</span>
                     </span>
                     <div className="background">
                       <div className="bar">
-                        <div
-                          className="bar_in"
-                          style={{ width: 90 + "%" }}
-                        ></div>
                       </div>
                     </div>
+                    <p style={{ marginTop: "5px" }} >React | Redux</p>
+                  </div>
+                  {/* End .progress_inner */}
+
+                  <div className="progress_inner">
+                    <span>
+                      <span className="label">Tech</span>
+                    </span>
+                    <div className="background">
+                      <div className="bar">
+                      </div>
+                    </div>
+                    <p style={{ marginTop: "5px" }} >Express | Node.js | Flask | Git | Heroku | Docker</p>
                   </div>
                   {/* End .progress_inner */}
                 </div>
--- src/views/all-home-version/HomeTwo.jsx
+++ src/views/all-home-version/HomeTwo.jsx
@@ -29,9 +29,6 @@ const HomeTwo = () => {
       <Skills />
       {/* End Skills Section */}
 
-      <Video />
-      {/* End Video Section */}
-
       <div className="shane_tm_section">
         <div className="shane_tm_partners">
           <div className="container">
@@ -41,38 +38,6 @@ const HomeTwo = () => {
           </div>
         </div>
       </div>
-      {/* End shane_tm_partners */}
-
-      <div className="shane_tm_section">
-        <div className="shane_tm_testimonials">
-          <div className="container">
-            <div className="testimonials_inner">
-              <div
-                className="left"
-                data-aos="fade-right"
-                data-aos-duration="1200"
-              >
-                <div className="shane_tm_title">
-                  <span>Testimonials</span>
-                  <h3>What clients say about my portfolio template</h3>
-                </div>
-              </div>
-
-              <div
-                className="right"
-                data-aos="fade-right"
-                data-aos-duration="1200"
-              >
-                <Testimonial />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* End  shane_tm_testimonials*/}
-
-      <News />
-      {/* End Blog Section */}
 
       <CallToAction />
       {/* End CallToAction */}
