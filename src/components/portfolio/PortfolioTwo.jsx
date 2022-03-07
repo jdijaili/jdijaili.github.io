cfa0eabda37a3f02f1690767b75f5282ef8fd4a9
jdijaili <jennifer.dijailirx@gmail.com>
2022-03-06 20:55:03 -0800

    updated portfolio

--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -3,118 +3,45 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import SimpleReactLightbox from "simple-react-lightbox";
 import { SRLWrapper } from "simple-react-lightbox";
 
-const tabList = ["All", "Design", "Branding", "Photography"];
+const tabList = ["All"];
 
 const tabListContent = [
   {
     porftoliItems: [
       {
         img: "1",
-        title: " My Story",
-        meta: "Design",
-        portfolioLink: "https://www.instagram.com/",
+        title: "Our Spot",
+        meta: "Github",
+        portfolioLink: "https://our-spot.herokuapp.com/",
         delayAnimation: "0",
+        githubLink: "https://github.com/jdijaili/our-spot-solo-capstone"
       },
       {
         img: "2",
-        title: "Design Trend",
-        meta: "Branding",
-        portfolioLink:
-          "https://dribbble.com/shots/16545341-Imroz-Vue-JS-Creative-Agency-Portfolio-Template",
+        title: "Teachables",
+        meta: "Github",
+        portfolioLink: "https://aa-group-project-teachables.herokuapp.com/",
         delayAnimation: "100",
-      },
-      {
-        img: "14",
-        title: "Soft Cream",
-        meta: "Photography",
-        portfolioLink:
-          "https://dribbble.com/shots/16529407-Deski-Saas-Software-React-Template",
-        delayAnimation: "200",
-      },
-      {
-        img: "22",
-        title: "Shane Design",
-        meta: "Design",
-        portfolioLink:
-          "https://dribbble.com/shots/16529282-Shane-Personal-Portfolio-React-Template",
-        delayAnimation: "0",
-      },
-      {
-        img: "7",
-        title: " Sweet Cherry",
-        meta: "Photography",
-        portfolioLink:
-          "https://dribbble.com/shots/16529226-Krozo-Personal-Portfolio-React-Template",
-        delayAnimation: "100",
+        githubLink: "https://github.com/jdijaili/w20-teachables-group-project"
       },
       {
         img: "3",
-        title: "Behance Shoot",
-        meta: "Branding",
-        portfolioLink: "https://www.behance.net/ib-themes",
+        title: "Sproutcloud",
+        meta: "Github",
+        portfolioLink: "https://sproutcloud.herokuapp.com/",
         delayAnimation: "200",
+        githubLink: "https://github.com/jdijaili/w16-sproutcloud-react-solo-project"
       },
-    ],
-  },
-  {
-    porftoliItems: [
       {
-        img: "22",
-        title: "Shane Design",
-        meta: "Design",
-        portfolioLink:
-          "https://dribbble.com/shots/16529282-Shane-Personal-Portfolio-React-Template",
+        img: "4",
+        title: "GoodMango",
+        meta: "Github",
+        portfolioLink: "https://goodmango.herokuapp.com/",
         delayAnimation: "0",
-      },
-      {
-        img: "7",
-        title: " Sweet Cherry",
-        meta: "Photography",
-        portfolioLink:
-          "https://dribbble.com/shots/16529226-Krozo-Personal-Portfolio-React-Template",
-        delayAnimation: "100",
-      },
+        githubLink: "https://github.com/jdijaili/w13-goodMango-group-project"
+      }
     ],
-  },
-  {
-    porftoliItems: [
-      {
-        img: "1",
-        title: " My Story",
-        meta: "Design",
-        portfolioLink: "https://www.instagram.com/",
-        delayAnimation: "0",
-      },
-      {
-        img: "2",
-        title: "Design Trend",
-        meta: "Branding",
-        portfolioLink:
-          "https://dribbble.com/shots/16545341-Imroz-Vue-JS-Creative-Agency-Portfolio-Template",
-        delayAnimation: "100",
-      },
-    ],
-  },
-  {
-    porftoliItems: [
-      {
-        img: "7",
-        title: " Sweet Cherry",
-        meta: "Photography",
-        portfolioLink:
-          "https://dribbble.com/shots/16529226-Krozo-Personal-Portfolio-React-Template",
-        delayAnimation: "0",
-      },
-      {
-        img: "15",
-        title: "Soft Cream",
-        meta: "Photography",
-        portfolioLink:
-          "https://dribbble.com/shots/16529407-Deski-Saas-Software-React-Template",
-        delayAnimation: "100",
-      },
-    ],
-  },
+  }
 ];
 
 const Portfolio = () => {
@@ -128,7 +55,7 @@ const Portfolio = () => {
                 <div className="title_flex">
                   <div className="left">
                     <span>Portfolio</span>
-                    <h3>Creative Portfolio</h3>
+                    <h3>Full Stack Projects</h3>
                   </div>
                 </div>
               </div>
@@ -174,7 +101,13 @@ const Portfolio = () => {
                                         {item.title}
                                       </a>
                                     </h3>
-                                    <span>{item.meta}</span>
+                                    <a
+                                      href={item.githubLink}
+                                      rel="noopener noreferrer"
+                                      target="_blank"
+                                    >
+                                      <span>{item.meta}</span>
+                                    </a>
                                   </div>
                                 </div>
                               </li>
